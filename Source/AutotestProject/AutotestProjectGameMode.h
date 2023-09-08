cad15a64e1d5825488db4d19f52e501da7209e2c
unknown <User@PC-Redin.corp.pangames.ru>
2023-09-08 17:04:34 +0300

    init

---
+++ Source/AutotestProject/AutotestProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AutotestProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAutotestProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAutotestProjectGameMode();
+};
+
+
+
